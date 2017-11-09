39dd3ab80961161cf886847066949e8120ffdf7f
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-08 23:55:54 -0800

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var myStuff = [121,234,33,44,"Jenna","Reag","Hello"];
+
 function sayHello(name){
   console.log("Hello: "+ name);
 }
@@ -15,9 +17,21 @@ function callHi(func,name){
   func(name);
 }
 
-callHi(sayHi,"Ravi");
+// callHi(sayHi,"Ravi");
 
 
 callHi(function(name){
   console.log("From Tell truth: "+ name);
 },"Jason");
+
+
+
+function myForEach(arr,callback){
+  for (var i = 0; i < arr.length; i++) {
+    callback(i,arr[i]);
+  }
+}
+
+myForEach(myStuff, function(idx,element) {
+    console.log(idx + " element is " + element);
+});
