02e497309af772e77b09f2ef5aeb84fc66793d54
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 01:06:44 -0800

    added temp

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 var index = require('./obj.js');
+var fs = require('fs');
 
 var p1 = new index.Person("name ","title ","state ",12);
 
 console.log(p1.getPerson());
+
+
+fs.readFileSync('./temp.txt','utf-8');
