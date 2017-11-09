4ebe827df22ab1087f59e6f9a014a730bda0ae51
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 11:35:32 -0800

    building response

--- inside_alexa/index.js
+++ inside_alexa/index.js
@@ -1,6 +1,23 @@
 exports.handler = function(event,context){
 
 	var request = event.request;
+	
+	if(request.type === "LaunchRequest"){
+
+	}else if(request.type === "IntentRequest"){
+
+	}else if(request.type === "SessionEndedRequest"){
+
+	}else{
+		context.fail("Unknown intent type");
+	}
+
+}
 
 
+function buildResponse(options){
+ var response = {
+
+	
+ };	
 }
