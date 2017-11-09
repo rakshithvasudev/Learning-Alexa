8473a9fe78641d3c8ef37e1508f0e3885ced5d73
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 11:52:16 -0800

    response object added

--- inside_alexa/index.js
+++ inside_alexa/index.js
@@ -3,9 +3,19 @@ exports.handler = function(event,context){
 	var request = event.request;
 	
 	if(request.type === "LaunchRequest"){
+		
+		context.succeed(buildResponse({
+			speechText:"Hello Welcome to greeting skill, I can greet you, just say the name.",
+			repromptText:" Who do you want to greet? You can say for example, Greet Rakshith",
+			endSession:false
+		}));
 
 	}else if(request.type === "IntentRequest"){
-
+		context.succeed(buildResponse({
+			speechText:"Hello",
+			repromptText:" Do you want to greet someone else as well?",
+			endSession:false
+		}));
 	}else if(request.type === "SessionEndedRequest"){
 
 	}else{
