83355b20d5bab621194d9ac0ec2d79e4b2d72a5e
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 01:13:38 -0800

    async

--- test.js
+++ test.js
@@ -6,10 +6,12 @@ var p1 = new index.Person("name ","title ","state ",12);
 console.log(p1.getPerson());
 
 
-// blocking code
 // var data = fs.readFileSync('./temp.txt','utf-8');
+// blocking code - sync code
 // console.log(data);
 
+
+// non blocking code - async code
 fs.readFile('./temp.txt','utf-8',function(err,data) {
   console.log(data);
 });
