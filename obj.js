f535870e3ae35165f4ebfc79953a4ec57e4dcaeb
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 00:51:06 -0800

    objects compile

--- obj.js
+++ obj.js
@@ -12,3 +12,20 @@ title: "Mr. ",
 state: "California",
 age: 22
 };
+
+class Person{
+  constructor(name,title,state,age) {
+    this.name = name;
+    this.title = title;
+    this.state = state;
+    this.age = age;
+  }
+
+  function getPerson() {
+    return title + name + state;
+  }
+}
+
+
+var rocky = new Person("Rocky", "Mr. ", " California", 23);
+console.log(rocky.getPerson());
