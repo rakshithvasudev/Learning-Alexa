73cb1c4ccdafeee9936c481367c449dcbb47802b
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 11:44:48 -0800

    response object added

--- inside_alexa/index.js
+++ inside_alexa/index.js
@@ -17,7 +17,25 @@ exports.handler = function(event,context){
 
 function buildResponse(options){
  var response = {
+	version: "1.0",
+	response: {
+		outputSpeech: {
+		  type: "PlainText",
+		  text: options.speechText
+		},
 
-	
- };	
+		shouldEndSession: options.endSession
+	  }
+	};	
+
+	if(options.repromptText){
+		response.response.reprompt = {
+			outputSpeech: {
+				type: "PlainText",
+				text: options.repromptText
+			  }
+		};
+	}
+
+	return response;
 }
