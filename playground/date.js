a1cb7dc7317ff025bbf752b4e7a4588f3d658404
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 12:43:22 -0800

    response object added

--- playground/date.js
+++ playground/date.js
@@ -3,3 +3,28 @@
 var mydate = new Date();
 
 console.log(mydate.getUTCHours()-8);
+
+
+function getWish(){
+	var currentHour = getCurrentHour();
+	if(currentHour<12){
+		return "Good Morning";
+	}else if(currentHour>=12 && currentHour < 18){
+		return "Good Afternoon";
+	} else{
+		return "Good Evening";
+	}
+}
+
+
+function getCurrentHour(){
+	var myDate = new Date();
+	var hours = myDate.getUTCHours()-8;
+	if(hours<0){
+		hours+=24;
+	}
+	return hours;
+}
+
+
+console.log(getWish());
