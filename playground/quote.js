fcba119d415df1464833699ead665a8e8f8ae57d
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-11-09 23:42:56 -0800

    response object added

--- playground/quote.js
+++ playground/quote.js
@@ -4,8 +4,19 @@ var http = require('http');
 function getQuote(callback){
   var url = "http://api.forismatic.com/api/1.0/json?method=getQuote&lang=en&format=json";
   var req = http.get(url,function(res){
-    
-  });  
+     var body = "";
+     
+     res.on("data",function(chunk){
+        body += chunk;
+     });
+     
+     res.on("end",function(){
+        body = body.replace(/\\/g,''); 
+        var quote = JSON.parse(body);
+        callback(quote.quoteText);
+     });
+
+    });  
 
   req.on("error",function(err){
     callback('',err);
